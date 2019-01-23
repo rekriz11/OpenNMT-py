799150e10a13fc49ceca7f43364bc5efc30b27c6
Reno Kriz <rekriz@seas.upenn.edu>
2019-01-23 16:24:32 -0500

    Bug fix

--- onmt/translate/translator.py
+++ onmt/translate/translator.py
@@ -665,7 +665,7 @@ class Translator(object):
                                     stepwise_penalty=self.stepwise_penalty,
                                     block_ngram_repeat=self.block_ngram_repeat,
                                     exclusion_tokens=exclusion_tokens,
-                                    prev_hyps=prev_hyps
+                                    prev_hyps=prev_hyps,
                                     hamming_dist=self.hamming_dist)
                 for __ in range(batch_size)]
 
