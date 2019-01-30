659c3658d128f9f42820750f14b8ee5b149d30b2
Reno Kriz <rekriz@seas.upenn.edu>
2019-01-30 18:37:19 -0500

    Fixx

--- onmt/translate/beam.py
+++ onmt/translate/beam.py
@@ -292,8 +292,8 @@ class Beam(object):
             best_scores = torch.from_numpy(np.array(scores_temp, dtype='double')).cuda()
             best_scores, scores_id = best_scores.topk(self.size, 0, True, True)
 
-            prev_k = torch.from_numpy(np.array([prev_k_temp[i].item() for i in scores_id], dtype='int32')).type(torch.LongTensor).cuda()
-            next_k = torch.from_numpy(np.array([next_k_temp[i].item() for i in scores_id], dtype='int32')).type(torch.LongTensor).cuda()
+            prev_k = torch.from_numpy(np.array([prev_k_temp[indices[i]].item() for i in scores_id], dtype='int32')).type(torch.LongTensor).cuda()
+            next_k = torch.from_numpy(np.array([next_k_temp[indices[i]].item() for i in scores_id], dtype='int32')).type(torch.LongTensor).cuda()
 
             ####### FOR DEBUGGING (DELETE LATER)
             print("\nBEAM AFTER DIVERSE BEAM SEARCH")
