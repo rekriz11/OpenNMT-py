477f2df3ee8fa52b9451ba2e48e7d7adc6dfe6fd
Reno Kriz <rekriz@seas.upenn.edu>
2019-02-20 15:57:04 -0500

    Debug

--- onmt/translate/translator.py
+++ onmt/translate/translator.py
@@ -236,6 +236,7 @@ class Translator(object):
         import json
         # Iterating over batches.
         for num, batch in enumerate(data_iter):
+            print("BATCH: " + str(num))
             ## Reinitialize previous hypotheses
             self.prev_hyps = []
             inputs = ["" for i in range(batch.batch_size)]
@@ -243,6 +244,7 @@ class Translator(object):
             scores = [[] for i in range(batch.batch_size)]
             # If doing iterative beam search, may run beam search multiple times.
             for i in range(self.beam_iters):
+                print("BEAM ITERATION:" + str(i))
                 batch_data = self.translate_batch(
                     batch, data, attn_debug, builder, fast=self.fast,
                 )
@@ -276,7 +278,9 @@ class Translator(object):
                         ## Checks if top candidate is empty (TODO: why is this happening?)
                         k = 0
                         while not trans.pred_sents[k]:
-                            k += 1 
+                            k += 1
+
+                        print(trans.pred_sents[k] + "\t" + str(float(trans.pred_scores[k])))
                         preds[j] += [trans.pred_sents[k]]
                         scores[j] += [float(trans.pred_scores[k])]
                         
